ef3091330c2748d0160a2b56b2f91b3a456489af
Konstantin Smetanin <Smetanin.K.S@citilink.ru>
2026-06-30 00:25:10 +0300

    wip teams controller

--- db/migrate/20260628181724_create_user_teams.rb
+++ db/migrate/20260628181724_create_user_teams.rb
@@ -3,7 +3,7 @@ class CreateUserTeams < ActiveRecord::Migration[8.1]
     create_table :user_teams, primary_key: [ :user_id, :team_id ] do |t|
       t.references :user, null: false, foreign_key: true
       t.references :team, null: false, foreign_key: true
-      t.integer :role, default: 3
+      t.integer :role, default: 1
 
       t.timestamps
     end
