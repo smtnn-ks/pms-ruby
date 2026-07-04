42da69ef3d2c09234c2ba9f76d7b2acf16091d5e
Konstantin Smetanin <smtnn.ks@gmail.com>
2026-07-04 15:25:24 +0300

    tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :get_team
+  before_action :get_task, only: %i[ show edit update destroy ]
+
   def show
-    @task = Task.find params[:id]
   end
 
   def new
@@ -8,16 +10,41 @@ class TasksController < ApplicationController
   end
 
   def create
-    @params = params.expect task: [ :title, :description ]
-    @task = Task.new @params
-    @task.team = Team.find params[:team_id]
+    @params = params.expect task: [ :title, :description, :assignee_id ]
+    @task = @team.tasks.new @params
     @task.author = Current.user
 
     if @task.save
-      pp @task
-      redirect_to team_task_path(params[:team_id], @task)
+      redirect_to team_task_path(@team, @task)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @params = params.expect task: [ :title, :description, :assignee_id ]
+    if @task.update @params
+      redirect_to team_task_path(@team, @task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to team_path(@team)
+  end
+
+  private
+
+  def get_team
+    @team = Current.user.teams.find params[:team_id]
+  end
+
+  def get_task
+    @task = @team.tasks.find params[:id]
   end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,152 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @task = tasks(:task_one)
+    @team = @task.team
+
+    assert_queries_count 4 do
+      sign_in_as(users(:user_one))
+    end
+  end
+
+  test "should show task" do
+    get team_task_url(@team, @task)
+    assert_response :success
+  end
+
+  test "should not show task if team is not found" do
+    get team_task_url(Team.new(id: 0), @task)
+    assert_response :not_found
+  end
+
+  test "should not show task if task is not found" do
+    get team_task_url(@team, Task.new(id: 0))
+    assert_response :not_found
+  end
+
+  test "should get new" do
+    get new_team_task_url(@team)
+    assert_response :success
+  end
+
+  test "should not get new if team is not found" do
+    get new_team_task_url(Team.new(id: 0))
+    assert_response :not_found
+  end
+
+  test "should create task" do
+    assert_difference("Task.count") do
+      post team_tasks_url(@team), params: { task: { title: "New Task", description: "Created from controller test" } }
+    end
+
+    @last_task = Task.last
+    assert_equal @last_task.author, users(:user_one)
+
+    assert_redirected_to team_task_url(@team, @last_task)
+  end
+
+  test "should create task with assignee" do
+    assert_difference("Task.count") do
+      post team_tasks_url(@team), params: { task: { title: "Assigned Task", description: "Task with assignee", assignee_id: users(:user_two).id } }
+    end
+
+    task = Task.last
+    assert_equal users(:user_two), task.assignee
+    assert_redirected_to team_task_url(@team, task)
+  end
+
+  test "should not create task if team is not found" do
+    assert_no_difference("Task.count") do
+      post team_tasks_url(Team.new(id: 0)), params: { task: { title: "New Task", description: "Created from controller test" } }
+    end
+
+    assert_response :not_found
+  end
+
+    test "should not create task with invalid params" do
+    assert_no_difference("Task.count") do
+      post team_tasks_url(@team), params: { task: { title: "", description: "" } }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test "should get edit" do
+    get edit_team_task_url(@team, @task)
+    assert_response :success
+  end
+
+  test "should not get edit if team is not found" do
+    get edit_team_task_url(Team.new(id: 0), @task)
+    assert_response :not_found
+  end
+
+  test "should not get edit if task is not found" do
+    get edit_team_task_url(@team, Task.new(id: 0))
+    assert_response :not_found
+  end
+
+  test "should update task" do
+    patch team_task_url(@team, @task), params: { task: { title: "Updated Task Title" } }
+    assert_redirected_to team_task_url(@team, @task)
+    @task.reload
+    assert_equal "Updated Task Title", @task.title
+  end
+
+  test "should update task assignee" do
+    patch team_task_url(@team, @task), params: { task: { assignee_id: users(:user_two).id } }
+
+    assert_redirected_to team_task_url(@team, @task)
+    @task.reload
+    assert_equal users(:user_two), @task.assignee
+  end
+
+  test "should not update task if team is not found" do
+    patch team_task_url(Team.new(id: 0), @task), params: { task: { title: "Updated Task Title" } }
+
+    assert_response :not_found
+    @task.reload
+    assert_not_equal "Updated Task Title", @task.title
+  end
+
+  test "should not update task if task is not found" do
+    patch team_task_url(@team, Task.new(id: 0)), params: { task: { title: "Updated Task Title" } }
+
+    assert_response :not_found
+    @task.reload
+    assert_not_equal "Updated Task Title", @task.title
+  end
+
+  test "should not update task with invalid params" do
+    patch team_task_url(@team, @task), params: { task: { title: "" } }
+
+    assert_response :unprocessable_entity
+    @task.reload
+    assert_not_equal "", @task.title
+  end
+
+  test "should destroy task" do
+    assert_difference("Task.count", -1) do
+      delete team_task_url(@team, @task)
+    end
+
+    assert_redirected_to team_url(@team)
+  end
+
+  test "should not destroy task if team is not found" do
+    assert_no_difference("Task.count") do
+      delete team_task_url(Team.new(id: 0), @task)
+    end
+
+    assert_response :not_found
+  end
+
+  test "should not destroy task if task is not found" do
+    assert_no_difference("Task.count") do
+      delete team_task_url(@team, Task.new(id: 0))
+    end
+
+    assert_response :not_found
+  end
 end
